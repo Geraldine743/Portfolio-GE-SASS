16d06a483e8b93df86af82701255efe58653cb73
Geraldine743 <egeraldine2@gmail.com>
2026-04-20 13:52:57 +0200

    add function fetchGitHubRepos()

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
+import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
+
 const sidenav = document.querySelector(".sidenav-js")
 const openBtn = document.getElementById("openBtn")
 const closeBtn = document.getElementById("closeBtn")
 const closeLinks = document.querySelectorAll(".closeLink")
+const githubContainer = document.getElementById('github-projects-container');
 
 
 openBtn.addEventListener('click', () => {
@@ -25,3 +28,27 @@ function openNav (){
 function closeNav (){
     sidenav.classList.remove("active")
 }
+
+async function fetchGitHubRepos() {
+    const octokit = new Octokit();
+
+    try {
+        const response = await octokit.rest.repos.listForUser({
+            username: 'Geraldine743', 
+            sort: 'updated',
+        });
+
+        const repos = response.data;
+        githubContainer.innerHTML = ''; 
+
+        repos.forEach(repo => {
+            const card = createRepoCard(repo);
+            githubContainer.appendChild(card);
+        });
+    } catch (error) {
+        console.error("Erreur lors de la récupération des repos:", error);
+        githubContainer.innerHTML = "<p>Impossible de charger les projets pour le moment.</p>";
+    }
+}
+
+fetchGitHubRepos();
